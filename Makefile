4bd632620dcdd61d215bf5c8e9fc579ab37478ac
VitAndrGuid <vitor.guidorizzi@hotmail.com>
2025-02-02 16:08:35 -0400

    .

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+# ---------------------- [GIT] ----------------------
 .PHONY: lazy
 lazy:
 	git add . && git commit -m "." && git push origin master
@@ -5,19 +6,23 @@ lazy:
 # ---------------------- [MAILER] ----------------------
 .PHONY: run_mailer_error
 run_mailer_error:
-	RUST_LOG=error AWS_PROFILE=rastercar-vitor cargo watch -x 'run -p mailer'
+	RUST_LOG=error cargo watch -x 'run -p mailer'
 
 .PHONY: run_mailer_warn
 run_mailer_warn:
-	RUST_LOG=warn AWS_PROFILE=rastercar-vitor cargo watch -x 'run -p mailer'
+	RUST_LOG=warn cargo watch -x 'run -p mailer'
 
 .PHONY: run_mailer_info
 run_mailer_info:
-	RUST_LOG=info AWS_PROFILE=rastercar-vitor cargo watch -x 'run -p mailer' 
+	RUST_LOG=info cargo watch -x 'run -p mailer' 
+
+.PHONY: run_mailer_dev
+run_mailer_dev:
+	RUST_LOG=info cargo watch -x 'run -p mailer'
 
 .PHONY: run_mailer_debug
 run_mailer_debug:
-	APP_DEBUG=true RUST_LOG=debug AWS_PROFILE=rastercar-vitor cargo watch -x 'run -p mailer'
+	APP_DEBUG=true RUST_LOG=debug cargo watch -x 'run -p mailer'
 
 # ---------------------- [DECODER] ----------------------
 .PHONY: run_decoder_error
--- services/mailer/src/config.rs
+++ services/mailer/src/config.rs
@@ -1,83 +1,32 @@
-use std::sync::OnceLock;
+use std::{env, sync::OnceLock};
 
+use config::{Config, Environment, File};
 use serde::Deserialize;
-
-fn def_app_debug() -> bool {
-    false
-}
-
-fn def_rmq_uri() -> String {
-    String::from("amqp://localhost:5672")
-}
-
-fn def_rmq_queue() -> String {
-    String::from("mailer")
-}
-
-fn def_rmq_consumer_tag() -> String {
-    String::from("mailer_service_consumer")
-}
-
-fn def_tracer_service_name() -> String {
-    String::from("mailer")
-}
-
-fn def_email_events_exchange() -> String {
-    String::from("email_events")
-}
-
-fn def_aws_region() -> String {
-    String::from("us-east-1")
-}
-
-fn def_app_default_email_sender() -> String {
-    String::from("rastercar.tests.001@gmail.com")
-}
-
-fn def_aws_ses_tracking_config_set() -> String {
-    String::from("email-events")
-}
-
-fn def_aws_ses_max_emails_per_second() -> u32 {
-    1
-}
-
-fn def_http_port() -> u16 {
-    3005
-}
 
 #[derive(Deserialize, Debug)]
 pub struct AppConfig {
     /// If the application should be run in debug mode and print additional info to stdout
-    #[serde(default = "def_app_debug")]
     pub app_debug: bool,
 
     /// The service name to be used on the tracing spans
-    #[serde(default = "def_tracer_service_name")]
     pub tracer_service_name: String,
 
     /// Rabbitmq uri
-    #[serde(default = "def_rmq_uri")]
     pub rmq_uri: String,
 
     /// Name of the rabbitmq queue this service will consume
-    #[serde(default = "def_rmq_queue")]
     pub rmq_queue: String,
 
     /// Tag name for the rabbitmq consumer of the queue in rmq_queue
-    #[serde(default = "def_rmq_consumer_tag")]
     pub rmq_consumer_tag: String,
 
     /// Name of the exchange to publish email events (clicks, opens, etc)
-    #[serde(default = "def_email_events_exchange")]
     pub rmq_email_events_exchange: String,
 
     /// AWS region
-    #[serde(default = "def_aws_region")]
     pub aws_region: String,
 
     /// Name of the SES configuration set to be used to track email events (clicks, opens, etc)
-    #[serde(default = "def_aws_ses_tracking_config_set")]
     pub aws_ses_tracking_config_set: String,
 
     /// AWS ARN of the SNS subscription used to publish tracked email events to this service,
@@ -87,25 +36,42 @@ pub struct AppConfig {
     /// Maximum amount of sendEmail operations per second for the AWS account.
     /// defaults to 1, the value for sandbox accounts
     /// see: https://docs.aws.amazon.com/ses/latest/dg/manage-sending-quotas.html
-    #[serde(default = "def_aws_ses_max_emails_per_second")]
     pub aws_ses_max_emails_per_second: u32,
 
-    #[serde(default = "def_http_port")]
+    /// HTTP port used to recieve SNS events
     pub http_port: u16,
 
     /// Email address to be used to send emails if the caller does not specify a address
-    #[serde(default = "def_app_default_email_sender")]
     pub app_default_email_sender: String,
 }
 
 impl AppConfig {
     pub fn from_env() -> AppConfig {
-        match envy::from_env::<AppConfig>() {
-            Ok(config) => config,
-            Err(error) => {
+        let run_mode = env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
+        let base_path = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+        // Start off by merging in the "default" configuration file
+        let s = Config::builder()
+            .add_source(
+                File::with_name(&format!("{base_path}/env/.{run_mode}.yaml"))
+                    .format(config::FileFormat::Yaml)
+                    .required(false),
+            )
+            .add_source(Environment::default())
+            .build()
+            .unwrap_or_else(|error| {
                 panic!("[CFG] failed to load application config, {:#?}", error)
-            }
-        }
+            });
+
+        println!("app_debug: {:?}", s.get_bool("app_debug"));
+        println!("APP_DEBUG: {:?}", s.get_bool("APP_DEBUG"));
+
+        s.try_deserialize::<AppConfig>().unwrap_or_else(|error| {
+            panic!(
+                "[CFG] failed to deserialize application config, {:#?}",
+                error
+            )
+        })
     }
 }
 
