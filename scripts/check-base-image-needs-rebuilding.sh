f7802c78ec2764a909c173aeaa3f72970c601c2e
VitAndrGuid <vitor.guidorizzi@hotmail.com>
2025-02-05 12:13:35 -0400

    .

---
+++ scripts/check-base-image-needs-rebuilding.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# checks if the base rust image used by the services needs to be rebuild
+
+## Did we found IP address? Use exit status of the grep command ##
+if git diff --exit-code -s HEAD~1 -- Dockerfile; then
+    echo "root Dockerfile not changed"
+    exit 0
+else
+    echo "root Dockerfile changed, base image needs rebuilding"
+    exit 1
+fi
