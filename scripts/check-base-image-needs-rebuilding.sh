29ec675805b83fdc9daf169fcbcc005ce4b335b6
VitAndrGuid <vitor.guidorizzi@hotmail.com>
2025-02-05 13:56:26 -0400

    .

--- scripts/check-base-image-needs-rebuilding.sh
+++ scripts/check-base-image-needs-rebuilding.sh
@@ -2,13 +2,11 @@
 
 # checks if the base rust image used by the services needs to be rebuild
 
-## Did we found IP address? Use exit status of the grep command ##
-if git diff --exit-code -s HEAD~1 -- Dockerfile; then
-    echo "root Dockerfile not changed"
-    exit 0
-else
-    echo "root Dockerfile changed, base image needs rebuilding"
-    exit 1
-fi
+for file in Dockerfile Cargo.lock Cargo.toml .dockerignore; do
+    if ! git diff --exit-code -s HEAD~1 -- "$file"; then
+        echo "$file changed, base image needs rebuilding"
 
-#
+        # returns 1 if rebuild is needed
+        exit 1
+    fi
+done
